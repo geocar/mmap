f07944e4869ec572819e2c42a909df9941c024a1
Geo Carncross <geocar@gmail.com>
2013-07-11 21:31:20 +0100

    more tests

--- run_tests.js
+++ run_tests.js
@@ -15,6 +15,8 @@ fs.truncateSync(fd, mmap.PAGESIZE);
 
 (function(b) {
   assert.equal(b[0],     t,             "value check (map read)");
+  assert.equal(b[1],     0,             "value check (line noise)");
+  assert.equal(b[2],     0,             "value check (line noise)");
   assert.equal(b.sync(), true,          "sync failed");
 
   assert.equal(b.length, mmap.PAGESIZE, "map length is incorrect");
@@ -27,4 +29,33 @@ fs.truncateSync(fd, mmap.PAGESIZE);
   mmap.MAP_SHARED, 
   fd) );
 
+fs.truncateSync(fd, 0);
+fs.truncateSync(fd, mmap.PAGESIZE);
+(function(b) {
+  assert.equal(b[0],     0,             "value check (truncated 0)");
+  assert.equal(b[1],     0,             "value check (always zero)");
+  assert.equal(b[2],     0,             "value check (always zero)");
+  assert.equal(b.length, mmap.PAGESIZE, "map length is incorrect");
+
+  b[2] = t;
+  assert.equal(b.sync(), true,          "sync failed");
+
+})( mmap.map(mmap.PAGESIZE,
+  mmap.PROT_READ|mmap.PROT_WRITE,
+  mmap.MAP_SHARED, 
+  fd) );
+
+
+(function(b) {
+  fs.closeSync(fd);
+
+  assert.equal(b[0],     0,             "value check (truncated 0)");
+  assert.equal(b[1],     0,             "value check (always zero)");
+  assert.equal(b[2],     t,             "third mapping");
+  assert.equal(b.length, mmap.PAGESIZE, "map length is incorrect");
+
+})( mmap.map(mmap.PAGESIZE,
+  mmap.PROT_READ,
+  mmap.MAP_SHARED, 
+  fd) );
 console.log("> ok");
